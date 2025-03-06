3ea0035a190ff9ea85e408586bba77a6919a3a2c
Juan Arillo <juan.arillo@apppadel.com>
2025-03-06 11:41:30 +0100

    feat: updated code

--- app/app.py
+++ app/app.py
@@ -4,30 +4,18 @@ import os
 
 app = Flask(__name__)
 
-# Configuración de Redis
+# Redis Configuration
 REDIS_HOST = os.getenv("REDIS_HOST", "redis")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_DB = int(os.getenv("REDIS_DB", 0))
 
+# Redis connection
 db = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
 
 @app.route('/')
-def get_all_keys():
-    keys = db.keys("*")
-    data = {key: db.get(key) for key in keys}
-    return jsonify(data)
+def index():
+    count = db.incr('hits')  # Increment redis counter
+    return f"La página ha sido cargada {count} veces."
 
-@app.route('/set/<key>/<value>', methods=['POST'])
-def set_value(key, value):
-    db.set(key, value)
-    return jsonify({"message": "Value set successfully"}), 201
-
-@app.route('/get/<key>', methods=['GET'])
-def get_value(key):
-    value = db.get(key)
-    if value:
-        return jsonify({"key": key, "value": value})
-    return jsonify({"message": "Key not found"}), 404
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
