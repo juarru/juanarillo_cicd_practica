551cc120301d94168919daa8537abda2434b1ccc
Juan Arillo <juan.arillo@apppadel.com>
2025-03-06 14:58:36 +0100

    test: added mock to simulate Redis

--- app/test_app.py
+++ app/test_app.py
@@ -1,10 +1,15 @@
 import pytest
-from app import app
+from app import app, db
+from unittest.mock import MagicMock
 
 @pytest.fixture
 def client():
     """Creates a Flask test client"""
     app.config["TESTING"] = True
+
+    # Mock Redis to return a fake counter value instead of connecting to a real Redis server
+    db.incr = MagicMock(return_value=1)
+
     with app.test_client() as client:
         yield client
 
@@ -20,9 +25,9 @@ def test_not_found(client):
     assert response.status_code == 404
 
 # Test that always fails intentionally to see what happens in pytest. Uncomment to see the result.
-def test_forced():
-    """Test that always fails intentionally"""
-    assert False, "This test is designed to fail"
+# def test_forced():
+#     """Test that always fails intentionally"""
+#     assert False, "This test is designed to fail"
 
 # Test to see what happens in coverage when a function is not tested. Uncomment to see the result.
 def test_no_tested_function():
